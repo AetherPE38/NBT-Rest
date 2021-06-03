607ab8710b12dbaf4ccdd979873cd7ee6530c89d
KingOfTurkey38 <ozomer792@gmail.com>
2021-06-03 20:02:55 +0200

    Remove unnecessary stuff

--- app/repositories.php
+++ app/repositories.php
@@ -6,8 +6,5 @@ use App\Infrastructure\Persistence\User\InMemoryUserRepository;
 use DI\ContainerBuilder;
 
 return function (ContainerBuilder $containerBuilder) {
-    // Here we map our UserRepository interface to its in memory implementation
-    $containerBuilder->addDefinitions([
-        UserRepository::class => \DI\autowire(InMemoryUserRepository::class),
-    ]);
+    $containerBuilder->addDefinitions([]);
 };
